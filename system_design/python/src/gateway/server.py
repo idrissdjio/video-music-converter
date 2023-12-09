f3df0136552a9a17147304d0f3e4539ff0957321
Idriss Djiofack <idrissdjiofack@Idrisss-MacBook-Pro.local>
2023-12-08 21:35:35 -0700

    solve the error uplaoding file by reinstalling mogodb locally

--- system_design/python/src/gateway/server.py
+++ system_design/python/src/gateway/server.py
@@ -1,19 +1,23 @@
 import os, gridfs, pika, json
-from flask import Flask, request
+from flask import Flask, request, send_file
 from flask_pymongo import PyMongo
 from auth import validate
 from auth_svc import access
 from storage import util
+from bson.objectid import ObjectId
 
 server = Flask(__name__)
-server.config["MONGO_URI"] = 'mongodb://host.minikube.internal:27017/videos'
 
-mongo = PyMongo(server)
+mongo_video = PyMongo(server, uri="mongodb://host.minikube.internal:27017/videos")
 
-fs = gridfs.GridFS(mongo.db) 
+mongo_mp3 = PyMongo(server, uri="mongodb://host.minikube.internal:27017/mp3s")
+
+fs_videos = gridfs.GridFS(mongo_video.db)
+fs_mp3s = gridfs.GridFS(mongo_mp3.db)
 
 connection = pika.BlockingConnection(pika.ConnectionParameters("rabbitmq"))
 channel = connection.channel()
+
 
 @server.route("/login", methods=["POST"])
 def login():
@@ -23,29 +27,56 @@ def login():
         return token
     else:
         return err
-    
+
+
 @server.route("/upload", methods=["POST"])
 def upload():
     access, err = validate.token(request)
+
+    if err:
+        return err
 
     access = json.loads(access)
 
     if access["admin"]:
         if len(request.files) > 1 or len(request.files) < 1:
             return "exactly 1 file required", 400
-        
+
         for _, f in request.files.items():
-            err = util.upload(f, fs, channel, access)
+            err = util.upload(f, fs_videos, channel, access)
 
             if err:
                 return err
+
         return "success!", 200
     else:
         return "not authorized", 401
-    
+
+
 @server.route("/download", methods=["GET"])
 def download():
-    pass
+    access, err = validate.token(request)
+
+    if err:
+        return err
+
+    access = json.loads(access)
+
+    if access["admin"]:
+        fid_string = request.args.get("fid")
+
+        if not fid_string:
+            return "fid is required", 400
+
+        try:
+            out = fs_mp3s.get(ObjectId(fid_string))
+            return send_file(out, download_name=f"{fid_string}.mp3")
+        except Exception as err:
+            print(err)
+            return "internal server error", 500
+
+    return "not authorized", 401
+
 
 if __name__ == "__main__":
     server.run(host="0.0.0.0", port=8080)
--- system_design/python/src/gateway/storage/util.py
+++ system_design/python/src/gateway/storage/util.py
@@ -1,11 +1,14 @@
 import pika, json
+import traceback
 
 def upload(f, fs, channel, access):
     try:
+        # Store file in GridFS
         fid = fs.put(f)
     except Exception as err:
-        return "internal server error", 500
-    
+        print("Error storing file in GridFS:", str(err), traceback.format_exc())
+        return "Error storing file", 500
+
     message = {
         "video_fid": str(fid),
         "mp3_fid": None,
@@ -13,14 +16,18 @@ def upload(f, fs, channel, access):
     }
 
     try:
+        # Publish message to RabbitMQ
         channel.basic_publish(
             exchange="",
             routing_key="video",
             body=json.dumps(message),
             properties=pika.BasicProperties(
-            delivery_mode=pika.spec.PERSISTENT_DELIVERY_MODE
+                delivery_mode=pika.spec.PERSISTENT_DELIVERY_MODE
             ),
         )
-    except:
-        fs.delete(fid)
-        return "Internal server error", 500
+    except Exception as err:
+        print("Error publishing to RabbitMQ:", str(err), traceback.format_exc())
+        fs.delete(fid)  # Clean-up by removing file from GridFS if publish fails
+        return "Error sending message", 500
+
+    return None  # No error
